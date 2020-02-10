3f554c6cc98413777dbec88441773bb9541f8560
Arli <arli.machado@gmail.com>
2020-02-10 17:32:59 -0300

    Definindo serviço de upload de arquivos

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import authMiddleware from './app/middlewares/auth';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
@@ -12,5 +16,7 @@ routes.use(authMiddleware);
 
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
