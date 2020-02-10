d76c3ab40e2967ccbbb37864054a920beaa7a83b
Arli <arli.machado@gmail.com>
2020-02-10 15:42:16 -0300

    Efetuando validação nos serviços de criação e atualizacao de schemas

--- backend/src/app/controllers/RecipientController.js
+++ backend/src/app/controllers/RecipientController.js
@@ -1,13 +1,32 @@
+import * as Yup from 'yup';
 import Recipient from '../models/Recipients';
+
+const recipientSchema = Yup.object().shape({
+  name: Yup.string().required(),
+  street: Yup.string().required(),
+  number: Yup.number().required(),
+  complements: Yup.string().required(),
+  state: Yup.string().required(),
+  city: Yup.string().required(),
+  postal_code: Yup.number().required(),
+});
 
 class RecipientController {
   async store(req, res) {
+    if (!(await recipientSchema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const recipient = await Recipient.create(req.body);
 
     return res.json(recipient);
   }
 
   async update(req, res) {
+    if (!(await recipientSchema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { id } = req.params;
 
     const recipient = await Recipient.findByPk(id);
