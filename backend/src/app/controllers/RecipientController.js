6b251fc22b442a253fdef01ca266357c1d4b947f
Arli <arli.machado@gmail.com>
2020-02-10 15:36:16 -0300

    Definindo serviço de atualizacao de destinatario

--- backend/src/app/controllers/RecipientController.js
+++ backend/src/app/controllers/RecipientController.js
@@ -6,6 +6,20 @@ class RecipientController {
 
     return res.json(recipient);
   }
+
+  async update(req, res) {
+    const { id } = req.params;
+
+    const recipient = await Recipient.findByPk(id);
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Recipient not found' });
+    }
+
+    const recipientUpdated = await recipient.update(req.body);
+
+    return res.json(recipientUpdated);
+  }
 }
 
 export default new RecipientController();
